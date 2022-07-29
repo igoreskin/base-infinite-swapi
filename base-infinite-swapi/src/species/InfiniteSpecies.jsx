ccb67fb5893f7da341770da2d81ff6890080482b
Igor Eskin <eskinhd@gmail.com>
2022-07-29 15:46:34 -0400

    People and species

--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -3,6 +3,7 @@ import { useInfiniteQuery } from "react-query";
 import { Species } from "./Species";
 
 const initialUrl = "https://swapi.dev/api/species/";
+
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
